3573ee3c5c08b91e638441f67f04b3de71d6d9f5
Kamil Świętoniowski <kamilswietoniowski91@gmail.com>
2026-09-08 21:29:27 +0200

    feat: add some components

--- src/main.rs
+++ src/main.rs
@@ -12,24 +12,38 @@ struct Position {
 }
 
 #[derive(Component)]
-struct Health {
-    hp: f32,
-}
-
-#[derive(Component)]
 struct Unit;
 
-fn main() {
-    App::new().add_systems(Startup, spawn_units).run();
+#[derive(Component)]
+struct Radio(String);
+
+#[derive(Component)]
+struct Health {
+    hp: f32,
+    max_health: f32,
 }
 
-fn spawn_units(mut commands: Commands) {
-    commands.spawn((
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, (setup, spawn_unit)).run();
+}
+
+fn spawn_unit(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let texture_handler = asset_server.load("infantry.jpeg");
+      commands.spawn((
+        Sprite::from_image(texture_handler),
+        Transform::default().with_scale(Vec3::splat(0.02)),
         Unit,
         Side {
             side: "Blufor".to_string(),
         },
         Position { x: 5.0, y: 4.0 },
-        Health { hp: 100.0 },
+        Health { hp: 100.0, max_health: 100.0 },
+        Radio("Alpha".to_string())
     ));
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn((Camera2d::default(), Position {x: 0.0, y: 0.0}));
 }

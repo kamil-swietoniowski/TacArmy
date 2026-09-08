0d3dee4e517f5e4a1f1225e079e0d8f5e1b10e6a
kamil-swietoniowski <kamilswietoniowski91@gmail.com>
2026-09-08 08:44:18 +0200

    feat: add some components

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+struct Side {
+    side: String,
+}
+
+#[derive(Component)]
+struct Position {
+    x: f32,
+    y: f32,
+}
+
+#[derive(Component)]
+struct Health {
+    hp: f32,
+}
+
+#[derive(Component)]
+struct Unit;
+
 fn main() {
-    println!("Hello, world!");
+    App::new().add_systems(Startup, spawn_units).run();
+}
+
+fn spawn_units(mut commands: Commands) {
+    commands.spawn((
+        Unit,
+        Side {
+            side: "Blufor".to_string(),
+        },
+        Position { x: 5.0, y: 4.0 },
+        Health { hp: 100.0 },
+    ));
 }
